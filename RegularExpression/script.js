34a32ddb3e8ab2c4506391d1c4ed00ff5ce0df74
dimaswntech <dimaswntech@gmail.com>
2024-01-23 10:44:06 +0700

    regex

---
+++ RegularExpression/script.js
@@ -0,0 +1,24 @@
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+
+//10
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+// 10
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+// 10
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+// 10
+
