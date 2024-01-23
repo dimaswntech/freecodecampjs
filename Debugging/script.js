edef0dc17dd594465490334b7c33ad6b7b506a9b
dimaswntech <dimaswntech@gmail.com>
2024-01-23 20:47:02 +0700

    debugging

--- Debugging/script.js
+++ Debugging/script.js
@@ -30,3 +30,14 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 
 //10
 
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+
+//10
+
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+//10
+
