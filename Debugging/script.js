0cf37c4a49b1505bf43b5572d3a45c19af0dfc4f
dimaswntech <dimaswntech@gmail.com>
2024-01-23 20:36:44 +0700

    regex

---
+++ Debugging/script.js
@@ -0,0 +1,32 @@
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.log(a);
+
+//10
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+console.clear()
+
+//10
+
+let seven = 7;
+let three = "3";
+console.log(typeof(seven));
+console.log(typeof(three));
+// Only change code below this line
+
+//10
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+//10
+
